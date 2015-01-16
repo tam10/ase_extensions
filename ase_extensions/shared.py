8e90ffe8ecd59e08f8ae2d5c329e4a5ace026886
Clyde-fare <clyde.fare@gmail.com>
2015-01-16 16:39:25 +0000

    Moved plaintext2html to shared.py
    This means we no longer depend on cc-utils

--- ase_extensions/shared.py
+++ ase_extensions/shared.py
@@ -1,5 +1,37 @@
 __author__ = 'clyde'
 
+import re
+
+def plaintext2html(text, tabstop=4):
+    #from http://djangosnippets.org/snippets/19/
+    re_string = re.compile(r'(?P<htmlchars>[<&>])|(?P<space>^[ \t]+)|(?P<lineend>\r\n|\r|\n)|(?P<protocal>(^|\s)((http|ftp)://.*?))(\s|$)', re.S|re.M|re.I)
+
+    def do_sub(m):
+        c = m.groupdict()
+        if c['htmlchars']:
+            return cgi.escape(c['htmlchars'])
+        if c['lineend']:
+            return '<br>'
+        elif c['space']:
+            t = m.group().replace('\t', '&nbsp;'*tabstop)
+            t = t.replace(' ', '&nbsp;')
+            return t
+        elif c['space'] == '\t':
+            return ' '*tabstop;
+        else:
+            url = m.group('protocal')
+            if url.startswith(' '):
+                prefix = ' '
+                url = url[1:]
+            else:
+                prefix = ''
+            last = m.groups()[-1]
+            if last in ['\n', '\r', '\r\n']:
+                last = '<br>'
+            return '%s<a href="%s">%s</a>%s' % (prefix, url, url, last)
+    return re.sub(re_string, do_sub, text)
+
+
 def to_molmod(ase_atoms):
     import molmod, tempfile, os
     from ase.io import write
